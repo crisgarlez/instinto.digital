1f27183e86598543326cd7f6648e4a5effe654da
Cristhian Garcia Vélez <escipion25@gmail.com>
2023-04-04 12:19:07 -0500

    Change blog status

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,23 +2,23 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Instinto Digital',
+  author: '@coninstinto',
+  headerTitle: 'Instinto Digital',
+  description: 'Digitales por Instinto',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://instinto.digital/',
+  siteRepo: '',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'digitalinstinto@gmail.com',
+  github: '',
+  twitter: 'https://twitter.com/coninstinto',
+  facebook: 'https://www.facebook.com/coninstinto',
+  youtube: '',
+  linkedin: '',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
